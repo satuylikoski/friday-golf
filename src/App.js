eac4e947086b6eb1c8e157fd3526c5299a7e4d9c
Satu Ylikoski <satu.ylikoski@freska.fi>
2020-02-29 14:10:19 +0200

    Check that changer has name in addition to ready

--- src/App.js
+++ src/App.js
@@ -27,7 +27,9 @@ function App() {
     Tabletop.init({
       key: process.env.REACT_APP_GOOGLE_KEY,
       callback: googleData => {
-        const filteredData = googleData.filter(data => data.ready === "TRUE");
+        const filteredData = googleData.filter(
+          data => data.ready === "TRUE" && data.name.length > 0
+        );
         setChangers(filteredData);
       },
       simpleSheet: true
